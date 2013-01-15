6394d80e9aced12fe0912b27ec610165a73b93a2
Gregory Haynes <greg@greghaynes.net>
2013-01-14 17:09:54 -0800

    Much more efficient processing by using Counter(), fix off by one error for culling stats

--- server.py
+++ server.py
@@ -5,44 +5,40 @@ from twisted.web import server, resource, static
 
 import pygeoip
 
-import json
-import collections
+import demjson
+from collections import deque, Counter
 
 class LocationStats(object):
 
     def __init__(self, geoip_db, minute_breakdowns=(1, 5, 15, 60)):
         self.geoip_db = geoip_db
-        self.cur_stats = collections.Counter()
+        self.cur_stats = Counter()
 
         self.minute_breakdowns = minute_breakdowns
         self.stats_by_minute = {}
         for i in minute_breakdowns:
-            self.stats_by_minute[i] = collections.Counter()
-        self.old_stats_queue = collections.deque()
+            self.stats_by_minute[i] = Counter()
+        self.old_stats_queue = deque()
         self.max_minute_breakdown = int(max(minute_breakdowns))
-
-        # Normalization factor
-        self.max_cnt = 1
-
-        self.min_count = 0
         reactor.callLater(60, self.next_minute)
+        self.min_count = 0
 
     def next_minute(self):
         self.min_count += 1
         for i in self.minute_breakdowns:
-            self.stats_by_minute[i] = self.stats_by_minute[i] + self.cur_stats
             if self.min_count >= i:
                 try:
                     sub_stats = self.old_stats_queue[i-1]
                 except IndexError:
-                    continue
+                    pass
                 else:
-                    self.stats_by_minute[i] = self.stats_by_minute[i] - self.old_stats_queue[i-1]
+                    self.stats_by_minute[i] = self.stats_by_minute[i] - sub_stats
+            self.stats_by_minute[i] = self.stats_by_minute[i] + self.cur_stats
 
         # Flip stats buffers
         self.old_stats_queue.appendleft(self.cur_stats)
-        self.cur_stats = collections.Counter()
-        if self.min_count >= self.max_minute_breakdown:
+        self.cur_stats = Counter()
+        if self.min_count > self.max_minute_breakdown:
             self.old_stats_queue.pop()
 
         try:
@@ -53,12 +49,12 @@ class LocationStats(object):
 
     def saw_addr(self, addr):
         rec = self.geoip_db.record_by_addr(addr)
+        if rec == None:
+            return
         self.cur_stats[(rec['latitude'], rec['longitude'])] += 1
 
         # set normalization factor
         cnt = self.cur_stats[(rec['latitude'], rec['longitude'])]
-        if cnt > self.max_cnt:
-            self.max_cnt = cnt
 
     def get_stats(self, min_breakdown=None):
         if min_breakdown == None:
@@ -72,15 +68,18 @@ class LocationStats(object):
         except AttributeError:
             pass
 
-        ret_data = []
+        ret_data = deque()
         for min_bd in self.minute_breakdowns:
-            set_data = []
+            set_data = deque()
+            try:
+                max_cnt = self.get_stats(min_bd).most_common(1)[0][1] * 1.0001
+            except IndexError:
+                ret_data.append([str(min_bd), set_data])
+                continue
             for loc, value in self.get_stats(min_bd).items():
-                set_data.append(loc[0])
-                set_data.append(loc[1])
-                set_data.append(value / (float(self.max_cnt) * 1.05))
+                set_data.extend((loc[0], loc[1], value / max_cnt))
             ret_data.append([str(min_bd), set_data])
-        self.json_stats_cached = json.dumps(ret_data)
+        self.json_stats_cached = demjson.encode(ret_data, encoding='ascii')
         return self.json_stats_cached
 
 
@@ -125,4 +124,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
