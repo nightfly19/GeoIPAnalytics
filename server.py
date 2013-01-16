259eab7ec9eb4972dcf4655d52b11159d9a90f2b
Gregory Haynes <greg@greghaynes.net>
2013-01-15 23:10:26 -0800

    Simplified code with single MultipleIntervalCounter class

--- server.py
+++ server.py
@@ -9,12 +9,37 @@ import pygeoip
 import demjson
 from collections import deque, Counter
 
+
+class MultiIntervalCounter(object):
+
+    def __init__(self, interval=1, multiples=(1,5,10)):
+        self.counter = Counter()
+        self.interval = interval
+        self.multiples = multiples
+        self.multi_counters = {}
+        self.counter_backlogs = {}
+        for i in multiples:
+            self.multi_counters[i] = Counter()
+            self.counter_backlogs[i] = deque()
+        reactor.callLater(interval, self.next_interval)
+
+    def next_interval(self):
+        for i, c in self.counter_backlogs.items():
+            if len(c) >= i:
+                self.multi_counters[i] -= c[i-1]
+            self.multi_counters[i] += self.counter
+            c.appendleft(self.counter)
+        self.counter = Counter()
+        reactor.callLater(self.interval, self.next_interval)
+
+
 class IPStats(object):
 
     def __init__(self, loc_stats):
         self.loc_stats = loc_stats
         self.seen_ips = Counter()
         self.bot_ips = Counter()
+        self.non_bot_ips = MultiIntervalCounter()
         self.resolver = createResolver()
 
     def saw_ip(self, ip):
@@ -23,6 +48,8 @@ class IPStats(object):
             d = self.resolver.lookupPointer('.'.join(ip.split('.')[::-1]) + '.in-addr.arpa')
             d.addCallback(self.ptr_response, ip)
             d.addErrback(self.ptr_error, ip)
+        if not self.is_bot(ip):
+            self.non_bot_ips.counter[ip] += 1
 
     def is_bot(self, addr):
         return self.bot_ips[addr] > 0
@@ -38,6 +65,7 @@ class IPStats(object):
                 or res.endswith('.google.com'):
             self.bot_ips[ip] += 1
             self.loc_stats.decrement_addr(ip)
+            self.non_bot_ips.counter[ip] -= 1
 
 
 class LocationStats(object):
@@ -45,46 +73,7 @@ class LocationStats(object):
     def __init__(self, geoip_db, minute_breakdowns=(1, 5, 15, 60)):
         self.geoip_db = geoip_db
         self.ip_stats = IPStats(self)
-        self.notable_ips = Counter()
-        self.cur_stats = Counter()
-        self.last_top_ips = str(demjson.encode([]))
-
-        self.minute_breakdowns = minute_breakdowns
-        self.stats_by_minute = {}
-        for i in minute_breakdowns:
-            self.stats_by_minute[i] = Counter()
-        self.old_stats_queue = deque()
-        self.max_minute_breakdown = int(max(minute_breakdowns))
-        reactor.callLater(60, self.next_minute)
-        self.min_count = 0
-
-    def next_minute(self):
-        self.min_count += 1
-        for i in self.minute_breakdowns:
-            if self.min_count >= i:
-                try:
-                    sub_stats = self.old_stats_queue[i-1]
-                except IndexError:
-                    pass
-                else:
-                    self.stats_by_minute[i] = self.stats_by_minute[i] - sub_stats
-            self.stats_by_minute[i] = self.stats_by_minute[i] + self.cur_stats
-
-        self.last_top_ips = str(demjson.encode(self.notable_ips.most_common(10)))
-
-        # Flip stats buffers
-        self.old_stats_queue.appendleft(self.cur_stats)
-        self.cur_stats = Counter()
-        self.notable_ips = Counter()
-        if self.min_count > self.max_minute_breakdown:
-            self.old_stats_queue.pop()
-
-        try:
-            del self.json_stats_cached
-        except AttributeError:
-            pass
-
-        reactor.callLater(60, self.next_minute)
+        self.stats = MultiIntervalCounter(multiples=minute_breakdowns)
 
     def saw_addr(self, addr):
         rec = self.geoip_db.record_by_addr(addr)
@@ -95,41 +84,15 @@ class LocationStats(object):
         if self.ip_stats.is_bot(addr):
             return
 
-        self.notable_ips[addr] += 1
-        self.cur_stats[(rec['latitude'], rec['longitude'])] += 1
+        self.stats.counter[(rec['latitude'], rec['longitude'])] += 1
 
     def decrement_addr(self, addr):
         rec = self.geoip_db.record_by_addr(addr)
         if rec == None:
             print 'This shouldnt happen, inval dec addr'
             return
-        self.cur_stats[(rec['latitude'], rec['longitude'])] -= 1
 
-    def get_stats(self, min_breakdown=None):
-        if min_breakdown == None:
-            return self.cur_stats
-        else:
-            return self.stats_by_minute[min_breakdown]
-
-    def get_globe_stats(self):
-        try:
-            return self.json_stats_cached
-        except AttributeError:
-            pass
-
-        ret_data = deque()
-        for min_bd in self.minute_breakdowns:
-            set_data = deque()
-            try:
-                max_cnt = self.get_stats(min_bd).most_common(1)[0][1] * 1.0001
-            except IndexError:
-                ret_data.append([str(min_bd), set_data])
-                continue
-            for loc, value in self.get_stats(min_bd).items():
-                set_data.extend((loc[0], loc[1], value / max_cnt))
-            ret_data.append([str(min_bd), set_data])
-        self.json_stats_cached = demjson.encode(ret_data, encoding='ascii')
-        return self.json_stats_cached
+        self.stats.counter[(rec['latitude'], rec['longitude'])] -= 1
 
 
 class GlobeStats(resource.Resource):
@@ -141,18 +104,30 @@ class GlobeStats(resource.Resource):
 
     def render_GET(self, request):
         request.setHeader("content-type", "application/json")
-        return self.loc_stats.get_globe_stats()
+        data = []
+        for i, c in self.loc_stats.stats.multi_counters.items():
+            vals = deque()
+            maxval = 0
+            try:
+                maxval = float(c.most_common(1)[0][1])
+            except IndexError:
+                pass
+            for loc, val in c.items():
+                vals.extend((loc[0], loc[1], val / maxval))
+            data.append((str(i), vals))
+        return str(demjson.encode(data))
+
 
 class TopIps(resource.Resource):
 
     isLeaf = True
 
-    def __init__(self, loc_stats):
-        self.loc_stats = loc_stats
+    def __init__(self, ip_stats):
+        self.ip_stats = ip_stats
 
     def render_GET(self, request):
         request.setHeader("content-type", "application/json")
-        return self.loc_stats.last_top_ips
+        return str(demjson.encode(self.ip_stats.non_bot_ips.multi_counters))
 
 
 class IpReceiver(DatagramProtocol):
@@ -176,7 +151,7 @@ def main():
     loc_stats = LocationStats(db)
     root_site = static.File("static")
     root_site.putChild("globe_stats.json", GlobeStats(loc_stats))
-    root_site.putChild("top_ips.json", TopIps(loc_stats))
+    root_site.putChild("top_ips.json", TopIps(loc_stats.ip_stats))
 
     reactor.listenTCP(8880, server.Site(root_site))
     reactor.listenUDP(8999, IpReceiver(loc_stats))
